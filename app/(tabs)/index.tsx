835ff32bddf7f5a5014f75c5c8333633cb660641
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-01-01 17:16:25 +0000

    feat: create a barebone UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,82 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Entypo from "@expo/vector-icons/Entypo";
+import React, { useState } from "react";
+
+import ExpenseForm from "@/components/ExpenseForm";
 
 export default function HomeScreen() {
+  const [isFormVisible, updateFormVisibility] = useState(false);
+  const [isPlusIconVisible, updatePlusIconVisibility] = useState(true);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.Container}>
+      <ImageBackground
+        source={require("@/assets/images/desert.jpeg")}
+        style={styles.BackgroundImage}
+      >
+        {isFormVisible ? <ExpenseForm></ExpenseForm> : <Text>" "</Text>}
+
+        {isPlusIconVisible ? (
+          <TouchableOpacity
+            style={styles.ButtonContainer}
+            onPress={() => {
+              updatePlusIconVisibility(false);
+              updateFormVisibility(true);
+            }}
+          >
+            <View style={styles.AddIconContainer}>
+              <Entypo name="add-to-list" size={40} color="white" />
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <Text>" "</Text>
+        )}
+      </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  Container: {
+    flex: 1,
+    justifyContent: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  BackgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  AddIconContainer: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "transparent",
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  ButtonContainer: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+  Title: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 26,
+    position: "absolute",
+    top: 30,
   },
 });
